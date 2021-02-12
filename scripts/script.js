ac48452b545c10d4627c4254f63c8941e789cc54
Jacob Khaliqi <jkhaliqi@ucsd.edu>
2021-02-11 22:24:23 -0800

    work on fetching array

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,7 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  let myFetch = fetch('https://fakestoreapi.com/products');
+  myFetch.then(response => response.json())
+  .then(commits => localStorage.setItem(commits[0].id, commits[0].title)); //slowly start figuring out how to fetch the array..
 });
