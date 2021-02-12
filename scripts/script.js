5916a3f4113b156187acf7753c6984ac7a17447f
Jacob Khaliqi <jkhaliqi@ucsd.edu>
2021-02-12 14:45:15 -0800

    work

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,16 @@
 // Script.js
-
+let list = document.getElementById("product-list");
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
   let myFetch = fetch('https://fakestoreapi.com/products');
   myFetch.then(response => response.json())
   .then(function(data) {
-    localStorage.clear();
-    for(i = 0; i < data.length; i++) {
-      localStorage.setItem(data[i].id,JSON.stringify(data[i]));
-    }
+      localStorage.setItem("productList",JSON.stringify(data));
+      for(i = 0; i < data.length; i++){
+        let productItem = document.createElement('product-item');
+        productItem.imgSrc=data[i].image;
+        productItem.title=data[i].title;
+        list.appendChild(productItem);
+      }
   })
 });
